ab7d4565764e90fbfed733ed64e32f23aae0c250
blackshibe <kacperszczepan@tutanota.com>
2024-09-24 22:18:47 +0200

    oops i can't release 0.23.0 without breaking all previous mods good job

--- src/index.tsx
+++ src/index.tsx
@@ -113,6 +113,7 @@ export namespace Modfile {
 let next_instance_id = 0;
 export namespace ModfilePackager {
 	// modifying binary data to change the version may have side effects, reexport your mods with the new version instead
+	// TODO: split actual packager version from this so that the version can change without breaking all the mods
 	export const PACKAGER_VERSION = "0.23.0-dev-3";
 
 	export function req_script_as<T>(root: Instance, name: string): T {
